bd95a4208f750a7282570322434c4c4d1ffa8120
mliamsinclair <m.liam.sinclair@gmail.com>
2024-02-05 20:25:52 -0500

    about section spacing

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -14,16 +14,16 @@ function About({ id, title }) {
         <p>
           I am a versatile software engineer with a passion for learning and a
           genuine love for development. Currently, I am a senior at
-          <span className="highlight">USC Upstate</span>, pursuing a
-          <span className="highlight">B.S.</span> with a dual major in
-          <span className="highlight">Computer Science</span> and
-          <span className="highlight">Cybersecurity</span>. The perfect blend of
-          creativity, logical thinking, and perpetual discovery fuels my passion
-          and excitement for innovative software development. I greatly enjoy
-          working in teams and am constantly seeking new opportunities to expand
-          my knowledge and skillset. I am actively seeking an
-          <span className="highlight">internship</span> for Spring 2024 or
-          <span className="highlight">full-time employment</span> following my
+          <span className="highlight"> USC Upstate </span>, pursuing a
+          <span className="highlight"> B.S. </span> with a dual major in
+          <span className="highlight"> Computer Science </span> and
+          <span className="highlight"> Cybersecurity </span>. The perfect blend
+          of creativity, logical thinking, and perpetual discovery fuels my
+          passion and excitement for innovative software development. I greatly
+          enjoy working in teams and am constantly seeking new opportunities to
+          expand my knowledge and skillset. I am actively seeking an
+          <span className="highlight"> internship </span> for Spring 2024 or
+          <span className="highlight"> full-time employment </span> following my
           graduation in April 2024.
         </p>
 
